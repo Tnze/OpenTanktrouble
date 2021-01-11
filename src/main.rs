469bcfc5a0a77fde5ca954616b1be6d6b7cce210
Tnze <cjd001113@outlook.com>
2021-01-11 13:00:18 +0800

    Move pipeline and vertex_buffer into maze object (doesn't work now)

--- src/main.rs
+++ src/main.rs
@@ -30,36 +30,6 @@ use winit::{
     window::{Fullscreen, Window, WindowBuilder},
 };
 
-mod vs {
-    vulkano_shaders::shader! {
-        ty: "vertex",
-        src: "
-				#version 450 core
-				layout(set = 0, binding = 0) uniform Data {
-                    vec2 trans;
-                } uniforms;
-				layout(location = 0) in vec2 position;
-
-				void main() {
-					gl_Position = vec4(position + uniforms.trans, 0.0, 1.0);
-				}
-			"
-    }
-}
-
-mod fs {
-    vulkano_shaders::shader! {
-        ty: "fragment",
-        src: "
-				#version 450 core
-				layout(location = 0) out vec4 f_color;
-				void main() {
-					f_color = vec4(1.0, 0.0, 0.0, 1.0);
-				}
-			"
-    }
-}
-
 fn main() {
     let required_extensions = vulkano_win::required_extensions();
     let instance = Instance::new(None, &required_extensions, None).unwrap();
@@ -122,79 +92,18 @@ fn main() {
     };
     // We now create a buffer that will store the shape of our triangle.
 
-    let (top, left, width, height) = (0.3, -0.7, 0.4, 0.2);
-
-    let uniform_buffer = CpuBufferPool::<vs::ty::Data>::new(device.clone(), BufferUsage::all());
-    let vs = vs::Shader::load(Arc::clone(&device)).unwrap();
-    let fs = fs::Shader::load(Arc::clone(&device)).unwrap();
-
-    let render_pass = Arc::new(
-        vulkano::single_pass_renderpass!(
-            Arc::clone(&device),
-            attachments: {
-                color: {
-                    load: Clear,
-                    store: Store,
-                    format: swapchain.format(),
-                    // TODO:
-                    samples: 1,
-                }
-            },
-            pass: {color: [color],  depth_stencil: {}}
-        )
-            .unwrap(),
+    let mut my_maze = maze::GameScene::create(
+        device.clone(),
+        swapchain.format(),
     );
-    let pipeline = Arc::new(
-        GraphicsPipeline::start()
-            .vertex_input_single_buffer()
-            .vertex_shader(vs.main_entry_point(), ())
-            .triangle_list()
-            .viewports_dynamic_scissors_irrelevant(1)
-            .fragment_shader(fs.main_entry_point(), ())
-            .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
-            .build(Arc::clone(&device))
-            .unwrap(),
-    );
-    let mut dynamic_state = DynamicState {
-        line_width: None,
-        viewports: None,
-        scissors: None,
-        compare_mask: None,
-        write_mask: None,
-        reference: None,
-    };
+
     let mut framebuffers =
-        window_size_dependent_setup(&images, render_pass.clone(), &mut dynamic_state);
+        window_size_dependent_setup(&images, my_maze.render.render_pass.clone(), &mut my_maze.render.dynamic_state);
 
     let mut recreate_swapchain = false;
 
-    #[derive(Default, Copy, Clone)]
-    struct Vertex {
-        position: (f32, f32),
-    }
-    vulkano::impl_vertex!(Vertex, position);
-
-    let vb = {
-        CpuAccessibleBuffer::from_iter(
-            Arc::clone(&device),
-            BufferUsage::all(),
-            false,
-            [
-                Vertex { position: (left, top) },
-                Vertex { position: (left + width, top) },
-                Vertex { position: (left + width, top + height) },
-                Vertex { position: (left, top) },
-                Vertex { position: (left + width, top + height) },
-                Vertex { position: (left, top + height) },
-            ]
-                .iter()
-                .cloned(),
-        )
-            .unwrap()
-    };
-
     let time_start = Instant::now();
-    let mut previous_frame_end = Some(sync::now(Arc::clone(&device)).boxed());
+    let mut previous_frame_end = Some(sync::now(device.clone()).boxed());
 
     // 初始化键盘控制器
     let keyboard_controller = Arc::new(Mutex::new(keyboard_controller::KeyboardController::new()));
@@ -209,7 +118,6 @@ fn main() {
         ],
     ));
 
-    let mut my_maze = maze::GameScene::create();
     my_maze.add_tank(sub_controller);
     let my_maze = Arc::new(my_maze);
     {
@@ -260,32 +168,12 @@ fn main() {
                     swapchain = new_swapchain;
                     framebuffers = window_size_dependent_setup(
                         &new_images,
-                        render_pass.clone(),
-                        &mut dynamic_state,
+                        my_maze.render.render_pass.clone(),
+                        &mut my_maze.render.dynamic_state,
                     );
                     recreate_swapchain = false;
                 }
 
-                let uniform_buffer_subbuffer = {
-                    let elapsed = time_start.elapsed();
-                    let elapsed =
-                        elapsed.as_secs() as f32 + elapsed.subsec_nanos() as f32 / 1_000_000_000.0;
-                    let trans = Vector2::new(0.5 + elapsed.sin() * 0.5, -0.5 + elapsed.cos() * 0.5);
-                    let uniform_data = vs::ty::Data {
-                        trans: trans.into(),
-                    };
-
-                    uniform_buffer.next(uniform_data).unwrap()
-                };
-
-                let layout = pipeline.layout().descriptor_set_layout(0).unwrap();
-                let set = Arc::new(
-                    PersistentDescriptorSet::start(layout.clone())
-                        .add_buffer(uniform_buffer_subbuffer)
-                        .unwrap()
-                        .build()
-                        .unwrap(),
-                );
 
                 let (image_num, suboptimal, acquire_future) =
                     match swapchain::acquire_next_image(swapchain.clone(), None) {
@@ -310,7 +198,7 @@ fn main() {
                     .unwrap();
 
                 builder.begin_render_pass(framebuffers[image_num].clone(), false, clear_values).unwrap();
-                my_maze.draw(&mut builder, pipeline.clone(), &dynamic_state, vb.clone(), set.clone()).unwrap();
+                my_maze.draw(&mut builder).unwrap();
                 builder.end_render_pass().unwrap();
 
 
--- src/maze.rs
+++ src/maze.rs
@@ -4,22 +4,30 @@ use rapier2d::{
         RigidBodySet,
     },
     geometry::{BroadPhase, ColliderSet, NarrowPhase},
-    na::{Vector2, Vector},
+    na::{Vector, Vector2},
     pipeline::PhysicsPipeline,
 };
 use std::sync::{Arc, Mutex};
-use std::{thread, time};
 use std::time::Duration;
-use vulkano::command_buffer::{AutoCommandBufferBuilder, DrawError, DynamicState};
+use std::{thread, time};
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer, CpuBufferPool};
 use vulkano::command_buffer::pool::standard::StandardCommandPoolBuilder;
+use vulkano::command_buffer::{AutoCommandBufferBuilder, DrawError, DynamicState};
+use vulkano::descriptor::descriptor_set::{DescriptorSetsCollection, PersistentDescriptorSet};
+use vulkano::device::Device;
+use vulkano::format::Format;
+use vulkano::framebuffer::{Subpass, RenderPass, RenderPassDesc, RenderPassAbstract};
+use vulkano::pipeline::vertex::{VertexSource, SingleBufferDefinition};
+use vulkano::pipeline::GraphicsPipeline;
 use vulkano::pipeline::GraphicsPipelineAbstract;
-use vulkano::pipeline::vertex::VertexSource;
-use vulkano::descriptor::descriptor_set::DescriptorSetsCollection;
+use vulkano::descriptor::PipelineLayoutAbstract;
+use vulkano::SafeDeref;
 
-pub struct GameScene {
+pub struct GameScene<RenderP>
+    where RenderP: RenderPassAbstract + Send + Sync {
     tanks: Vec<Tank>,
     physical: Arc<Mutex<PhysicalStatus>>,
-    render: RenderObjects,
+    pub(crate) render: RenderObjects<Box<dyn PipelineLayoutAbstract + Send + Sync>, Arc<RenderP>>,
 }
 
 struct PhysicalStatus {
@@ -34,10 +42,117 @@ struct PhysicalStatus {
     joint_set: JointSet,
 }
 
-struct RenderObjects {}
+struct RenderObjects<Layout, RenderP>
+    where RenderP: RenderPassAbstract + Send + Sync,
+          Layout: PipelineLayoutAbstract + Send + Sync {
+    pub(crate) dynamic_state: DynamicState,
+    pub(crate) pipeline: Arc<GraphicsPipeline<SingleBufferDefinition<Vertex>, Layout, RenderP>>,
+    pub(crate) render_pass: RenderP,
+    uniform_buffer: CpuBufferPool::<vs::ty::Data>,
+    vertex_buffer: Arc<CpuAccessibleBuffer<[Vertex]>>,
+}
 
-impl GameScene {
-    pub fn create() -> GameScene {
+mod vs {
+    vulkano_shaders::shader! {
+        ty: "vertex",
+        src: "
+				#version 450 core
+				layout(set = 0, binding = 0) uniform Data {
+                    vec2 trans;
+                } uniforms;
+				layout(location = 0) in vec2 position;
+
+				void main() {
+					gl_Position = vec4(position + uniforms.trans, 0.0, 1.0);
+				}
+			"
+    }
+}
+
+mod fs {
+    vulkano_shaders::shader! {
+        ty: "fragment",
+        src: "
+				#version 450 core
+				layout(location = 0) out vec4 f_color;
+				void main() {
+					f_color = vec4(1.0, 0.0, 0.0, 1.0);
+				}
+			"
+    }
+}
+
+#[derive(Default, Copy, Clone)]
+struct Vertex {
+    position: (f32, f32),
+}
+vulkano::impl_vertex!(Vertex, position);
+
+impl<RenderP> GameScene<RenderP>
+    where RenderP: PipelineLayoutAbstract + Send + Sync {
+    pub fn create(device: Arc<Device>, format: Format) -> GameScene<RenderP> {
+        let vs = vs::Shader::load(Arc::clone(&device)).unwrap();
+        let fs = fs::Shader::load(Arc::clone(&device)).unwrap();
+
+        let render_pass = Arc::new(
+            vulkano::single_pass_renderpass!(
+                device,
+                attachments: {
+                    color: {
+                        load: Clear,
+                        store: Store,
+                        format: format,
+                        // TODO:
+                        samples: 1,
+                    }
+                },
+                pass: {color: [color],  depth_stencil: {}}
+            ).unwrap(),
+        );
+        let pipeline = Arc::new(
+            GraphicsPipeline::start()
+                .vertex_input_single_buffer()
+                .vertex_shader(vs.main_entry_point(), ())
+                .triangle_list()
+                .viewports_dynamic_scissors_irrelevant(1)
+                .fragment_shader(fs.main_entry_point(), ())
+                .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+                .build(device.clone())
+                .unwrap(),
+        );
+        let layout = pipeline.layout();
+
+        let (top, left, width, height) = (0.3, -0.7, 0.4, 0.2);
+        let uniform_buffer = CpuBufferPool::<vs::ty::Data>::new(device.clone(), BufferUsage::all());
+        let mut dynamic_state = DynamicState {
+            line_width: None,
+            viewports: None,
+            scissors: None,
+            compare_mask: None,
+            write_mask: None,
+            reference: None,
+        };
+
+
+        let vertex_buffer = {
+            CpuAccessibleBuffer::from_iter(
+                device.clone(),
+                BufferUsage::all(),
+                false,
+                [
+                    Vertex { position: (left, top) },
+                    Vertex { position: (left + width, top) },
+                    Vertex { position: (left + width, top + height) },
+                    Vertex { position: (left, top) },
+                    Vertex { position: (left + width, top + height) },
+                    Vertex { position: (left, top + height) },
+                ]
+                    .iter()
+                    .cloned(),
+            )
+                .unwrap()
+        };
+
         GameScene {
             tanks: Vec::new(),
             physical: Arc::new(Mutex::new(PhysicalStatus {
@@ -49,7 +164,13 @@ impl GameScene {
                 collider_set: ColliderSet::new(),
                 joint_set: JointSet::new(),
             })),
-            render: RenderObjects {},
+            render: RenderObjects {
+                dynamic_state,
+                pipeline,
+                uniform_buffer,
+                render_pass,
+                vertex_buffer,
+            },
         }
     }
 
@@ -58,7 +179,12 @@ impl GameScene {
         let right_body = RigidBodyBuilder::new(BodyStatus::Dynamic)
             .can_sleep(true)
             .build();
-        let physical_handle = self.physical.lock().unwrap().rigid_body_set.insert(right_body);
+        let physical_handle = self
+            .physical
+            .lock()
+            .unwrap()
+            .rigid_body_set
+            .insert(right_body);
         self.tanks.push(Tank {
             controller,
             physical_handle,
@@ -68,7 +194,9 @@ impl GameScene {
     pub fn run_physic(&self) {
         let start_time = time::Instant::now();
         let mut pipeline = PhysicsPipeline::new();
-        let dt = time::Duration::from_secs_f32(self.physical.lock().unwrap().integration_parameters.dt());
+        let dt = time::Duration::from_secs_f32(
+            self.physical.lock().unwrap().integration_parameters.dt(),
+        );
         let gravity = Vector2::new(0.0, 0.0);
         loop {
             let physical = &mut *self.physical.lock().unwrap();
@@ -99,24 +227,30 @@ impl GameScene {
         }
     }
 
-    pub fn draw<'a, V, S, Gp>(
+    pub fn draw<'a>(
         &self,
         builder: &'a mut AutoCommandBufferBuilder,
-        pipeline: Gp,
-        dynamic_state: &DynamicState,
-        vertex_buffer: V,
-        sets: S,
-    ) -> Result<&'a mut AutoCommandBufferBuilder<StandardCommandPoolBuilder>, DrawError>
-        where Gp: GraphicsPipelineAbstract + VertexSource<V> + Send + Sync + 'static + Clone,
-              S: DescriptorSetsCollection
-    {
-        builder.draw(
-            pipeline,
-            &dynamic_state,
-            vertex_buffer,
-            sets,
-            (),
-        )
+    ) -> Result<&'a mut AutoCommandBufferBuilder<StandardCommandPoolBuilder>, DrawError> {
+        let uniform_buffer_subbuffer = {
+            // let elapsed = time_start.elapsed();
+            // let elapsed =
+            //     elapsed.as_secs() as f32 + elapsed.subsec_nanos() as f32 / 1_000_000_000.0;
+            // let trans = Vector2::new(0.5 + elapsed.sin() * 0.5, -0.5 + elapsed.cos() * 0.5);
+            let trans = Vector2::new(0.0, 0.0);
+            let uniform_data = vs::ty::Data {
+                trans: trans.into(),
+            };
+            self.render.uniform_buffer.next(uniform_data).unwrap()
+        };
+        let layout = self.render.pipeline.layout().descriptor_set_layout(0).unwrap();
+        let set = Arc::new(
+            PersistentDescriptorSet::start(layout.clone())
+                .add_buffer(uniform_buffer_subbuffer)
+                .unwrap()
+                .build()
+                .unwrap(),
+        );
+        builder.draw(&self.render.pipeline, &self.render.dynamic_state, &self.render.vertex_buffer, set, ())
     }
 }
 
